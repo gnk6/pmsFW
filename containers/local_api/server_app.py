c89af7c2bd59513f279b83434f8ee57c96fca33a
ggiannakopoulos <ggiannakopoulos@navarino.gr>
2025-02-06 19:51:14 +0200

    removed comments

--- containers/local_api/server_app.py
+++ containers/local_api/server_app.py
@@ -12,7 +12,6 @@ servapp = Flask(__name__)
 
 server_int = {'pnet':'br0', 'lnet':'br1', 'cnet':'br2'}
 def create_container_net(vlan):
-    #try:
     vlan_id = vlan['interface'].split('_')[1]
     parent = vlan['parent']
     srvint = server_int[parent]+'.'+vlan_id
@@ -31,8 +30,6 @@ def create_container_net(vlan):
         if 'Error' in msg:
             raise Exception(f'Could not delete vlan. {msg}')
     return msg
-    #except Exception as error:
-    #return error
 
 @servapp.route('/vlanconfig', methods=['POST'])
 def generate_vlan():
